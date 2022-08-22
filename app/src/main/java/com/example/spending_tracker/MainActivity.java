ca733c3557b0bce21d6320dc50e86af0291988ce
IrisTheSnail <salwa999adlouni@gmail.com>
2022-08-22 23:19:06 +0100

    ayo that layout who will create tiself is ready, yaaaaay

--- app/src/main/java/com/example/spending_tracker/MainActivity.java
+++ app/src/main/java/com/example/spending_tracker/MainActivity.java
@@ -1,32 +1,24 @@
 package com.example.spending_tracker;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.EditText;
+//import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.spending_tracker.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    //TextView textView;
+    EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputText = (EditText) findViewById(R.id.inputText1);
     }
+
+
 
 }
